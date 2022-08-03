764ec1a7132a8e6037ed044df61583fbf7d09db3
Will <103394466+OhDangBro@users.noreply.github.com>
2022-08-03 10:18:50 -0400

    saving

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -3,6 +3,7 @@ const sequelize = require('../config/connection');
 const { Post, User, Comment } = require('../models');
 
 router.get('/', (req, res) => {
+    console.log(req.session);
     Post.findAll({
       attributes: [
         'id',
@@ -38,4 +39,17 @@ router.get('/', (req, res) => {
       });
   });
 
+  router.get('/login', (req, res) => {
+    if (req.session.loggedIn) {
+        {
+      res.redirect('/');
+      return;
+    }
+  
+    res.render('login');
+  }});
+
 module.exports = router;
+
+
+//STOPPED AT 14.2.6/////
